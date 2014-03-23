8866fdc3ff0d6946230c152d7b6cc942c40feafd
Colby Aley <colby@aley.me>
2014-03-23 15:18:08 -0700

    Updating site

---
+++ javascripts/application-11810a0b.js
@@ -0,0 +1,1 @@
+function toggleExtra(){var e=document.getElementsByClassName("extra")[0],a=document.getElementById("more");"block"==e.style.display?(e.style.display="none",a.innerHTML="Read more."):(e.style.display="block",e.className="extra animated fadeIn",a.innerHTML="Read less.")}
